d0b6e1b2fe352300b409fa3952ffb8168ea49323
RodrigoGomez2 <91531538+RodrigoGomez2@users.noreply.github.com>
2023-05-09 17:20:46 +0200

    Add files via upload

--- DemoFinal/haproxy.py
+++ DemoFinal/haproxy.py
@@ -1,5 +1,7 @@
 import asyncio
 import telegram
+import subprocess
+import re
 
 # Configura el token del bot y el chat ID de destino
 bot_token = '5723725362:AAHM-OxcJdcZSBEN-DA4wXSE13PPmND3irE'
@@ -7,39 +9,46 @@ chat_id = '-929326885'
 
 # Crea el objeto bot
 bot = telegram.Bot(token=bot_token)
+process = None
+command = "sudo docker-compose up" #comando a monitorizar
+process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-last_message = ""
+async def send_command_output():
+    last_message = ""
+    server_start_count = 33
+    sending_count = 0
+    message = ""
 
-async def send_logs():
-    global last_message
+    while True:
+        output = process.stdout.readline().decode().strip()
+        print(output)
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            if output == last_message:
+                continue
+            if server_start_count > 0 and ("haproxy" in output or "backend" in output):
+                server_start_count -= 1
+            if server_start_count == 0 and ("haproxy" in output or "backend" in output) :
+                if sending_count !=2 and (server_start_count == 0 and ("haproxy" in output or "backend" in output)):
+                    sending_count += 1
+                    message = message + "\n" + output
+                if sending_count == 2 and (server_start_count == 0 and ("haproxy" in output or "backend" in output)):
+                    sending_count = 0
+                    try:
+                        await bot.send_message(chat_id=chat_id, text=message)
+                        message = ""
+                    except telegram.error.TimedOut:
+                        print("Timed out while sending message. Retrying in next iteration.")
+                        await asyncio.sleep(10)
+            last_message = output
 
-    # Lee el contenido del archivo de log
-    with open('logfile.txt', 'r') as f:
-        log_text = f.read()
-
-    # Si el texto del log es diferente al último mensaje enviado, enviar el mensaje
-    if log_text != last_message:
-        # Divide el texto en líneas y envía cada línea como un mensaje separado
-        log_lines = log_text.split('\n')
-        for line in log_lines:
-            if line.strip() != "":
-                try:
-                    await bot.send_message(chat_id=chat_id, text=line)
-                except telegram.error.TimedOut:
-                    print("Timed out while sending message. Retrying in next iteration.")
-
-        # Actualiza la variable last_message
-        last_message = log_text
-
-    # Borra el contenido del archivo de log
-    open('logfile.txt', 'w').close()
-    await asyncio.sleep(10)
-
+    await asyncio.sleep(1)
 
 while True:
     try:
         # Llama a la función asincrónica
-        asyncio.run(send_logs())
+        asyncio.run(send_command_output())
     except Exception as e:
         print(f"Error: {e}. Retrying in next iteration.")
-    # Espera 10 segundos antes de volver a llamar a la función
+
